58f6617eb7d6197f4aeb28ebfc4888728c72abea
Chinmaydg98 <chiinmaydgawande1998@gmail.com>
2021-12-14 21:37:13 +0530

    Implemented User Registration

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
+from . import views
 
 app_name = 'users'  # This is the namespace
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls'))
+    path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 
 # Create your views here.
 def index(request):
     """Homepage"""
     return render(request, 'users/index.html')
+
+
+def register(request):
+    """User registration"""
+    if request.method != 'POST':
+        # First request means just display registration form
+        form = UserCreationForm()
+    else:
+        # Means registration data received, now: Validate, Register User & Login
+        form = UserCreationForm(data=request.POST)
+
+        # Validate
+        if form.is_valid():
+            # Register User
+            new_user = form.save()
+
+            # Login
+            login(request, new_user)
+            return redirect('index')
+
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
